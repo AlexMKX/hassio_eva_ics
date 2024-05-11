5d5c0a731b1656bd18a8da2203e7599bd741d12d
Alex MKX <github-alex@xxl.cx>
2024-05-11 18:22:51 +0400

    Added CJ-YBT water level sensor

---
+++ eva_ics/hassio/modbus2mqtt/devices/cj_ybt/cj_ybt.py
@@ -0,0 +1,53 @@
+from DeviceConfig import DeviceConfig
+from typing import List, Any, Dict
+
+
+class Device(DeviceConfig):
+
+    def initialize(self):
+        """
+        Initializes the discovery - set manufacturer and model.
+        """
+        self.manufacturer = "ZGCJ"
+        self.model = "CJ-YBT"
+
+    @property
+    def puller_config(self) -> List[Dict[str, Any]]:
+        """
+        Builds puller configuration for EvaICS.
+
+        :return: puller configuration
+        """
+        return [
+            {
+                "reg": "i4",
+                "unit": self.slave,
+                "count": 1,
+                "map": [
+                    {
+                        "offset": 0,
+                        "oid": f"sensor:{self.eva_id}/level",
+                        "type": "int16",
+                    },
+                ]
+
+            }
+        ]
+
+    @property
+    def discovery(self) -> List[Dict[str, Any]]:
+        """
+
+        Building the discovery message for sensors.
+
+        :return: Discovery data
+        """
+        rv = [
+            self.build_discovery(item_type="sensor",
+                                 payload={"water_level": "{{ value_json.value | is_defined }}",
+                                          "state_class": "measurement",
+                                          "device_class": "distance",
+                                          "unit_of_meas": "cm",
+                                          },
+                                 slug="level")]
+        return rv
---
+++ eva_ics/hassio/modbus2mqtt/devices/cj_ybt/index.rst
@@ -0,0 +1,11 @@
+CJ-YBT
+========
+.. image:: ./device.jpg
+
+.. autopydantic_settings:: devices.xy_md02.xy_md02.Device
+   :members:
+
+.. literalinclude:: cj_ybt.py
+   :language: python
+   :caption: CJ-YBT Water Level sensor
+
